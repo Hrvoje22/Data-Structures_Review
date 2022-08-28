b1dd9dd84dc4e2c753e120559127b12d65f736a4
Hrvoje <hbrkljacic@gmail.com>
2022-08-28 16:33:51 -0400

    01

---
+++ 01.AlgorithmAnalysis/src/ArrayDemo.java
@@ -0,0 +1,17 @@
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+
+
+        int[] scoreArray = new int[10];
+        System.out.println(scoreArray);
+
+
+
+
+    }
+
+
+
+}
+
