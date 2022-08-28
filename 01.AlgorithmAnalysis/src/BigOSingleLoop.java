71fc6b2a458ce87ccd03466f4916e5f471781c2c
Hrvoje <hbrkljacic@gmail.com>
2022-08-28 13:24:00 -0400

    01

--- 01.AlgorithmAnalysis/src/BigOSingleLoop.java
+++ 01.AlgorithmAnalysis/src/BigOSingleLoop.java
@@ -1,7 +1,7 @@
 public class BigOSingleLoop {
     public static void main(String[] args) {
         int numberOfOperations=0;
-        int n=100;// size of  Data
+        int n=1000000;// size of  Data
         long startTime;
         long endTime;
         System.out.println("Input size n is = "+ n);
