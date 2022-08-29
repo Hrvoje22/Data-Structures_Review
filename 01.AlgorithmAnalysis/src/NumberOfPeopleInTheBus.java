c36d68701d2c09706009ad432c4b235ad96f01e2
Hrvoje <hbrkljacic@gmail.com>
2022-08-29 11:46:31 -0400

    NumOfPeopleInTheBusSolution

---
+++ 01.AlgorithmAnalysis/src/NumberOfPeopleInTheBus.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class NumberOfPeopleInTheBus {
+
+    public static void main(String[] args) {
+
+        ArrayList<int[]> busStops = new ArrayList<int[]>();
+        busStops.add(new int[] {10,0}); // first stop //10 got in //0 got off
+        busStops.add(new int[] {3,5});  // second stop
+        busStops.add(new int[] {2,5});  // third stop
+
+        System.out.println(countPassengers(busStops));
+
+
+    }
+
+    public static int countPassengers(ArrayList<int[]> stops){
+
+        int numOfPeople = 0;
+
+        for (int i = 0; i < stops.size(); i++) {
+            numOfPeople = numOfPeople + stops.get(i)[0] - stops.get(i)[1];
+        }
+
+        return numOfPeople;
+    }
+
+}
