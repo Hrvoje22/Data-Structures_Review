bbb4e467c6fa3b48cd0bf08aa03747c9031bf3dc
Hrvoje <hbrkljacic@gmail.com>
2022-08-29 15:33:14 -0400

    Map

--- 02.CollectionsReview/src/MapReview.java
+++ 02.CollectionsReview/src/MapReview.java
@@ -9,14 +9,37 @@ public class MapReview {
         studentsMap.put(1,"Jack");
         studentsMap.put(2,"Julia");
 
+        String str = "JJavvaDeveloper";
+
+        System.out.println(findFirstNonRepeating(str));
+
 
     }
     public static Character findFirstNonRepeating(String str){
 
-        //create a map : this will cost me space complexity
+        //create a map : this will cost me space complexity O(n)
+
+        Map<Character,Integer> map = new HashMap<>();
+
+        int count;
+
+        //count the frequency of chars
+
+        for (Character ch : str.toCharArray()){
+            if(map.containsKey(ch)){
+                count = map.get(ch);
+                map.put(ch,count + 1);
+            }else{
+                map.put(ch,1);
+            }
+        }
 
 
+        // start from the beginning and check if there is a char with frequency 1
 
+        for (Character ch : str.toCharArray()){
+            if(map.get(ch)==1) return ch;
+        }
 
         return null;
     }
