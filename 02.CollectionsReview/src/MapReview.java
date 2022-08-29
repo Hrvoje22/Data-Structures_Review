94d8735a201e540594f65e4e77d91b59f7dfb1cf
Hrvoje <hbrkljacic@gmail.com>
2022-08-29 15:16:20 -0400

    Map

---
+++ 02.CollectionsReview/src/MapReview.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapReview {
+    public static void main(String[] args) {
+    // create hash map
+
+        Map<Integer,String> studentsMap = new HashMap<>();
+        studentsMap.put(1,"Jack");
+        studentsMap.put(2,"Julia");
+
+
+    }
+    public static Character findFirstNonRepeating(String str){
+
+        //create a map : this will cost me space complexity
+
+
+
+
+        return null;
+    }
+}
