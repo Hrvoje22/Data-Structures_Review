6d4f91881fa1889feb780454dcf1177c38b82c84
Hrvoje <hbrkljacic@gmail.com>
2022-09-09 16:45:08 -0400

    LinkedList

--- 03.LinkedLists/src/MySinglyLinkedList.java
+++ 03.LinkedLists/src/MySinglyLinkedList.java
@@ -62,6 +62,24 @@ public class MySinglyLinkedList {
 
         }
 
+        //ASSIGNMENT 1
+        int indexOf(int id){
+
+            if(isEmpty()) return -1;
+
+            int pos=0;
+            // iterate through the list
+            Node current = head; //set my current with the starting element
+            while(current !=null){
+                if(current.id==id) return pos;
+                pos++;
+                current=current.next;
+            }
+
+            return -1;
+
+        }
+
         void printNode () {
             Node current = head;
             while (current != null) {
