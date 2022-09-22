43806453050c28e589947df1a56ebf14f202c9c4
Hrvoje <hbrkljacic@gmail.com>
2022-09-22 18:16:13 +0200

    SunsetViews

---
+++ 04.Stacks/src/sunsetViews/SunsetViewsEast.java
@@ -0,0 +1,26 @@
+package sunsetViews;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class SunsetViewsEast {
+    public static void main(String[] args) {
+        int[] buildings={3, 5, 4, 4, 3, 1, 3, 2};
+        sunsetViews(buildings,"EAST").forEach(System.out::println);
+    }
+    public static ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < buildings.length; i++) {
+            while(!stack.isEmpty() && buildings[i]>=buildings[stack.peek()]){
+                stack.pop();
+            }
+            stack.push(i);
+        }
+
+        //now I have a stack with teh buildings indices that can see the sunset
+
+
+        return new ArrayList<Integer>(stack);
+    }
+}
---
+++ 04.Stacks/src/sunsetViews/SunsetViewsWest.java
@@ -0,0 +1,39 @@
+package sunsetViews;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+public class SunsetViewsWest {
+    public static void main(String[] args) {
+        int[] buildings={3, 5, 4, 4, 3, 1, 3, 2};
+        sunsetViews(buildings,"West").forEach(System.out::println);
+    }
+    public static ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
+
+        Stack<Integer> stack = new Stack<>();
+        int i=0;
+        int step=1;
+        //this part is for west
+
+        if(direction.equalsIgnoreCase("west")){
+            i = buildings.length-1;
+            step=-1;
+        }
+
+        while(i>=0 && i< buildings.length){
+
+            while(!stack.isEmpty() && buildings[i]>=buildings[stack.peek()]){
+                stack.pop();
+            }
+            stack.push(i);
+            i+=step;
+        }
+
+        //now I have a stack with teh buildings indices that can see the sunset
+
+        if(direction.equalsIgnoreCase("west")) Collections.reverse(stack);
+
+        return new ArrayList<Integer>(stack);
+    }
+}
