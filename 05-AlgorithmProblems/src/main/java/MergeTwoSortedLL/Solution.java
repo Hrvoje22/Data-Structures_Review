4abee56704c2cb954c34f3af16abce6d360c6901
Hrvoje <hbrkljacic@gmail.com>
2022-09-23 12:38:33 +0200

    MergeTwoSortedLL

--- 05-AlgorithmProblems/src/main/java/MergeTwoSortedLL/Solution.java
+++ 05-AlgorithmProblems/src/main/java/MergeTwoSortedLL/Solution.java
@@ -1,9 +1,34 @@
 package MergeTwoSortedLL;
 
 public class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    public ListNode mergeTwoLists(ListNode curr1, ListNode curr2) {
 
-        return list1;
+        //crate a dummy head Node;
+
+        ListNode tempHead = new ListNode();
+        ListNode prev = tempHead;
+        while(curr1!=null && curr2!=null) {
+            if (curr1.val <= curr2.val) {
+                prev.next = curr1;
+                curr1 = curr1.next;
+
+            } else {
+                prev.next = curr2;
+                curr2 = curr2.next;
+            }
+
+            prev = prev.next;
+
+        }
+        // check if any leftover
+
+        if(curr1==null) prev.next = curr2;
+        else prev.next = curr1;
+
+
+        return tempHead.next;
+
+
 
     }
 }
