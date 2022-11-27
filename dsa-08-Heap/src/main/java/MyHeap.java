a42fa90736b0a9486bc97b27d88ebb59f508662e
Hrvoje <hbrkljacic@gmail.com>
2022-11-26 22:10:49 -0500

    HeapMy

--- dsa-08-Heap/src/main/java/MyHeap.java
+++ dsa-08-Heap/src/main/java/MyHeap.java
@@ -17,4 +17,39 @@ public class MyHeap {
 
     }
 
+    public void insert(int value){
+        if(size== items.length) throw new IllegalStateException(); //we cannot insert new element here
+        else{
+            items[size++]=value;
+            bubbleUp();
+        }
+
+    }
+
+    public void bubbleUp(){
+        //calculate start index/point
+        int index = size - 1;
+        //while index > 0 && parent is less than child continue
+        //1. swap child and parent
+        //2. calculate next index = parent index
+        while(index>0 && items[index]>items[parentIndex(index)]){
+            //swap
+            swap(index,parentIndex(index));
+            //assign index with the parent index
+            index = parentIndex(index); //like current=current.next
+        }
+
+
+    }
+
+    public int parentIndex(int index){
+        return (index-1)/2;
+    }
+
+    public void swap(int first, int second){
+        int temp = items[first];
+        items[first] = items[second];
+        items[second] = temp;
+    }
+
 }
