6b0159e592b2b9807bd1dbd68e5adf91dd79aad4
Hrvoje <hbrkljacic@gmail.com>
2022-11-26 22:11:45 -0500

    HeapMy

--- dsa-08-Heap/src/main/java/MyHeap.java
+++ dsa-08-Heap/src/main/java/MyHeap.java
@@ -52,4 +52,12 @@ public class MyHeap {
         items[second] = temp;
     }
 
+
+    public void printHeap(){
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i] + ", ");
+        }
+        System.out.println();
+    }
+
 }
